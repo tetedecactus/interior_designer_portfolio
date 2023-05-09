d57d07c315918c32f552e84b589ba6bffad49fc0
Olivier <o.labrecque.lacasse@gmail.com>
2023-05-09 13:33:24 -0400

    ___

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Option1 from './option1/Option1';
-import Navbar from './Navbar';
+// import Navbar from './Navbar';
 
 
 function App() {
   return (
       <Router>
         <div className="App">
-          <Navbar />
+          {/* <Navbar /> */}
           <Routes>
             <Route path="/" element={<Option1 />} />
           </Routes>
--- src/option1/Page3.js
+++ src/option1/Page3.js
@@ -1,14 +1,14 @@
 function Page3() {
     return (
-        <div className="page3-box">
-            <div className="flex flex-col">
-                <h4 className=" justify-self-end mb-24 text-4xl uppercase tracking-wider font-thin">table des matières</h4>
-                <div className=" flex flex-col space-y-6">
-                    <p className="text-left text-4xl tracking-widest">01  <span className="text-2xl uppercase font-thin border-b border-dashed border-black">la cuisine thibodeau</span></p>
-                    <p className="text-left text-4xl tracking-widest">02  <span className="text-2xl uppercase font-thin border-b border-dashed border-black">la résidence garneau</span></p>
-                    <p className="text-left text-4xl tracking-widest">03  <span className="text-2xl uppercase font-thin border-b border-dashed border-black">le meuble lavabo</span></p>
-                    <p className="text-left text-4xl tracking-widest">04  <span className="text-2xl uppercase font-thin border-b border-dashed border-black">le stand d'exposition</span></p>
-                    <p className="text-left text-4xl tracking-widest">05  <span className="text-2xl uppercase font-thin border-b border-dashed border-black">l'hotel</span></p>
+        <div className="page3-box flex flex-col justify-center items-center">
+            <div className="flex flex-col justify-center items-center">
+                <h4 className=" text-right mb-24 text-4xl uppercase tracking-wider font-thin">table des matières</h4>
+                <div className=" flex flex-col  space-y-6">
+                    <p className="text-left text-4xl tracking-widest">01  <span className="text-2xl pr-28 pb-1 uppercase font-thin border-b border-dashed border-black">la cuisine thibodeau</span></p>
+                    <p className="text-left text-4xl tracking-widest">02  <span className="text-2xl pr-28 pb-1 uppercase font-thin border-b border-dashed border-black">la résidence garneau</span></p>
+                    <p className="text-left text-4xl tracking-widest">03  <span className="text-2xl pr-28 pb-1 uppercase font-thin border-b border-dashed border-black">le meuble lavabo</span></p>
+                    <p className="text-left text-4xl tracking-widest">04  <span className="text-2xl pr-28 pb-1 uppercase font-thin border-b border-dashed border-black">le stand d'exposition</span></p>
+                    <p className="text-left text-4xl tracking-widest">05  <span className="text-2xl pr-28 pb-1 uppercase font-thin border-b border-dashed border-black">l'hotel</span></p>
                 </div>
             </div>
         </div>
